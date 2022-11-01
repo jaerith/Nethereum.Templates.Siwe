b07baaf2be09cd73d06e97194a38ac9ddf857797
jaerith <Aaron.Kendall@gmail.com>
2022-10-31 22:51:31 -0400

    Initial revision

---
+++ ExampleProjectSiwe.SiweRecap/SiweRecapCapability.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+using Nethereum.Siwe.Core;
+
+namespace ExmpleProjectSiwe.SiweRecap
+{
+    public class SiweRecapCapability : SiweMessage
+    {
+        public HashSet<string> DefaultActions { get; protected set; }
+
+        public Dictionary<string, HashSet<string>> TargetedActions { get; protected set; }
+
+        public Dictionary<string, string> ExtraFields { get; protected set; }
+
+        public SiweRecapCapability(HashSet<string> defaultActions,
+               Dictionary<string, HashSet<string>> targetedActions,
+                        Dictionary<string, string> extraFields)
+        {
+            DefaultActions  = defaultActions;
+            TargetedActions = targetedActions;
+            ExtraFields     = extraFields;
+        }
+    }
+}
