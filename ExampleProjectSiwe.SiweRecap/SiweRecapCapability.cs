9c918ab60d3ba35f53ef91a4044d11166a76abc2
jaerith <Aaron.Kendall@gmail.com>
2022-11-03 18:10:48 -0400

    * Made necessary changes to structures. * Added ToStatementText() method.

--- ExampleProjectSiwe.SiweRecap/SiweRecapCapability.cs
+++ ExampleProjectSiwe.SiweRecap/SiweRecapCapability.cs
@@ -1,37 +1,76 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
-namespace ExmpleProjectSiwe.SiweRecap
+namespace ExampleProjectSiwe.SiweRecap
 {
     public class SiweRecapCapability
     {
-        public HashSet<string> DefaultActions { get; protected set; }
+        private readonly HashSet<string> _DefaultActions;
 
-        public Dictionary<string, HashSet<string>> TargetedActions { get; protected set; }
+        private readonly Dictionary<SiweNamespace, HashSet<string>> _TargetedActions;
 
-        public Dictionary<string, string> ExtraFields { get; protected set; }
+        private readonly Dictionary<string, string> _ExtraFields;
 
         public SiweRecapCapability(HashSet<string> defaultActions,
-               Dictionary<string, HashSet<string>> targetedActions,
+        Dictionary<SiweNamespace, HashSet<string>> targetedActions,
                         Dictionary<string, string> extraFields)
         {
-            DefaultActions  = defaultActions;
-            TargetedActions = targetedActions;
-            ExtraFields     = extraFields;
+            _DefaultActions  = defaultActions;
+            _TargetedActions = targetedActions;
+            _ExtraFields     = extraFields;
         }
 
-        public bool HasPermissionByTarget(string target, string action)
+        public bool HasPermissionByTarget(SiweNamespace siweNamspace, string action)
         {
             HashSet<string>? targetActions = null;
 
-            return TargetedActions.TryGetValue(target, out targetActions) &&
+            return _TargetedActions.TryGetValue(siweNamspace, out targetActions) &&
                    (HasPermissionByDefault(action) || targetActions.Any(x => x.ToLower() == action.ToLower()));
         }
 
         public bool HasPermissionByDefault(string action)
         {
-            return DefaultActions.Any(x => x.ToLower() == action.ToLower());
+            return _DefaultActions.Any(x => x.ToLower() == action.ToLower());
         }
 
+        public HashSet<string> ToStatementText(SiweNamespace siweNamespace)
+        {
+            var textSectionBuilder      = new StringBuilder();
+            var capabilityTextLines     = new HashSet<string>();
+            var defaultActionsFormatted = new HashSet<string>();
+            var actionsFormatted        = new HashSet<string>();
 
+            _DefaultActions
+                .ToList()
+                .ForEach(x => defaultActionsFormatted.Append(FormatAction(siweNamespace, x)));
+
+            foreach (var siweNamespaceKey in _TargetedActions.Keys)
+            {
+                _TargetedActions[siweNamespaceKey]
+                    .ToList()
+                    .ForEach(x => actionsFormatted.Append(FormatAction(siweNamespaceKey, x)));
+            }
+
+            if (defaultActionsFormatted.Count > 0)
+            {
+                capabilityTextLines.Add(String.Join(", ", defaultActionsFormatted));
+            }
+
+            if (actionsFormatted.Count > 0)
+            {
+                capabilityTextLines.Add(String.Join(", ", actionsFormatted));
+            }
+
+            return capabilityTextLines;
+        }
+
+        #region Static Methods
+
+        static public string FormatAction(SiweNamespace siweNamespace, string defaultAction)
+        {
+            return string.Format("{0}: {1} for any.", siweNamespace, defaultAction);
+        }
+
+        #endregion 
     }
 }
